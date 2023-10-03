cb286638bb8e48828bf5edb08ebe95379091757c
Youssouf Oumar <63708012+yousoumar@users.noreply.github.com>
2023-10-03 16:36:00 +0200

    Fix movies service best movie route (#5)

--- movie/movie.py
+++ movie/movie.py
@@ -6,34 +6,46 @@ from werkzeug.exceptions import NotFound
 app = Flask(__name__)
 
 PORT = 3200
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 
-with open('{}/databases/movies.json'.format("."), "r") as jsf:
-   movies = json.load(jsf)["movies"]
+with open("{}/databases/movies.json".format("."), "r") as jsf:
+    movies = json.load(jsf)["movies"]
+
 
 # root message
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response(
+        "<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200
+    )
 
-@app.route("/template", methods=['GET'])
+
+@app.route("/template", methods=["GET"])
 def template():
-    return make_response(render_template('index.html', body_text='This is my HTML template for Movie service'),200)
+    return make_response(
+        render_template(
+            "index.html", body_text="This is my HTML template for Movie service"
+        ),
+        200,
+    )
 
-@app.route("/json", methods=['GET'])
+
+@app.route("/json", methods=["GET"])
 def get_json():
     res = make_response(jsonify(movies), 200)
     return res
 
-@app.route("/movies/<movieid>", methods=['GET'])
+
+@app.route("/movies/<movieid>", methods=["GET"])
 def get_movie_byid(movieid):
     for movie in movies:
         if str(movie["id"]) == str(movieid):
-            res = make_response(jsonify(movie),200)
+            res = make_response(jsonify(movie), 200)
             return res
-    return make_response(jsonify({"error":"Movie ID not found"}),400)
+    return make_response(jsonify({"error": "Movie ID not found"}), 400)
 
-@app.route("/moviesbytitle", methods=['GET'])
+
+@app.route("/moviesbytitle", methods=["GET"])
 def get_movie_bytitle():
     json = ""
     if request.args:
@@ -43,54 +55,61 @@ def get_movie_bytitle():
                 json = movie
 
     if not json:
-        res = make_response(jsonify({"error":"movie title not found"}),400)
+        res = make_response(jsonify({"error": "movie title not found"}), 400)
     else:
-        res = make_response(jsonify(json),200)
+        res = make_response(jsonify(json), 200)
     return res
 
-@app.route("/movies/<movieid>", methods=['POST'])
+
+@app.route("/movies/<movieid>", methods=["POST"])
 def create_movie(movieid):
     req = request.get_json()
 
     for movie in movies:
         if str(movie["id"]) == str(movieid):
-            return make_response(jsonify({"error":"movie ID already exists"}),409)
+            return make_response(jsonify({"error": "movie ID already exists"}), 409)
 
     movies.append(req)
-    res = make_response(jsonify({"message":"movie added"}),200)
+    res = make_response(jsonify({"message": "movie added"}), 200)
     return res
 
-@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+
+@app.route("/movies/<movieid>/<rate>", methods=["PUT"])
 def update_movie_rating(movieid, rate):
     for movie in movies:
         if str(movie["id"]) == str(movieid):
             movie["rating"] = int(rate)
-            res = make_response(jsonify(movie),200)
+            res = make_response(jsonify(movie), 200)
             return res
 
-    res = make_response(jsonify({"error":"movie ID not found"}),201)
+    res = make_response(jsonify({"error": "movie ID not found"}), 201)
     return res
 
-@app.route("/movies/<movieid>", methods=['DELETE'])
+
+@app.route("/movies/<movieid>", methods=["DELETE"])
 def del_movie(movieid):
     for movie in movies:
         if str(movie["id"]) == str(movieid):
             movies.remove(movie)
-            return make_response(jsonify(movie),200)
+            return make_response(jsonify(movie), 200)
 
-    res = make_response(jsonify({"error":"movie ID not found"}),400)
+    res = make_response(jsonify({"error": "movie ID not found"}), 400)
     return res
 
+
 @app.route("/movies/best", methods=["GET"])
-def rating_process()
-    rating=0
-    dic= {}
-    for rate in jsf:
-        if rate["rating"]>rating:
-            rating=rate["rating"]
-            dic=rate
-    return jsonify(dic)
+def rating_process():
+    rating = 0
+    bestMovie = {}
+    for movie in movies:
+        if movie["rating"] > rating:
+            rating = movie["rating"]
+            bestMovie = movie
+
+    return make_response(jsonify(bestMovie), 200)
+
+
 if __name__ == "__main__":
-    #p = sys.argv[1]
-    print("Server running in port %s"%(PORT))
+    # p = sys.argv[1]
+    print("Server running in port %s" % (PORT))
     app.run(host=HOST, port=PORT)
